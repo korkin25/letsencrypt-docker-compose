a3883170f1129bc69ac8e641c54c82ee5ff0e021
kk573 <28926495+korkin25@users.noreply.github.com>
2023-08-09 12:07:41 +0700

    cli.sh AI optimised

--- cli.sh
+++ cli.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 usage() {
-      cat << EOF
-Usage: $(basename $0) COMMAND
+    cat << EOF
+Usage: $(basename "$0") COMMAND
 
   config  [OPTIONS]          Run the CLI tool
           --no-current-user  Run as root (Docker default) instead of the current user
@@ -16,63 +16,89 @@ EOF
     exit 1
 }
 
-case $1 in
-  config)
-    CMD=(docker-compose run --rm cli)
+run_cli() {
+    docker-compose run --rm cli
+}
+
+run_docker_compose_up() {
+    docker-compose up -d
+}
+
+run_docker_compose_build() {
+    docker-compose --profile config build "${@}"
+}
+
+handle_config_command() {
+    CMD=run_cli
     CURRENT_USER="$(id -u):$(id -g)"
     DOCKER_GROUP="$(getent group docker | cut -d: -f3)"
     export CURRENT_USER DOCKER_GROUP
     shift
-    while [[ $# -gt 0 ]]; do
-      case $1 in
-        --no-current-user)
-          unset CURRENT_USER
-          unset DOCKER_GROUP
-          shift
-          ;;
-        *)
-          echo "Unknown option $1"
-          usage
-          ;;
-      esac
-    done
-    ;;
-  up)
-    CMD=(docker-compose up -d)
-    shift
-    while [[ $# -gt 0 ]]; do
-      case $1 in
-        --dry-run)
-          export DRY_RUN=true
-          shift
-          ;;
-        *)
-          echo "Unknown option $1"
-          usage
-          ;;
-      esac
-    done
-    ;;
-  build)
-    CMD=(docker-compose --profile config build)
-    shift
-    while [[ $# -gt 0 ]]; do
-      case $1 in
-        --no-cache)
-          CMD+=(--no-cache)
-          shift
-          ;;
-        *)
-          echo "Unknown option $1"
-          usage
-          ;;
-      esac
-    done
-    ;;
-  *)
-    echo "Unknown command $1"
-    usage
-    ;;
-esac
 
-"${CMD[@]}"
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+            --no-current-user)
+                unset CURRENT_USER
+                unset DOCKER_GROUP
+                shift
+                ;;
+            *)
+                echo "Unknown option $1"
+                usage
+                ;;
+        esac
+    done
+
+    "${CMD}"
+}
+
+handle_up_command() {
+    CMD=run_docker_compose_up
+    shift
+
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+            --dry-run)
+                export DRY_RUN=true
+                shift
+                ;;
+            *)
+                echo "Unknown option $1"
+                usage
+                ;;
+        esac
+    done
+
+    "${CMD}"
+}
+
+handle_build_command() {
+    CMD=run_docker_compose_build
+    shift
+
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+            --no-cache)
+                shift
+                CMD+=(--no-cache)
+                ;;
+            *)
+                echo "Unknown option $1"
+                usage
+                ;;
+        esac
+    done
+
+    "${CMD[@]}"
+}
+
+main() {
+    case $1 in
+        config) handle_config_command ;;
+        up) handle_up_command ;;
+        build) handle_build_command ;;
+        *) echo "Unknown command $1"; usage ;;
+    esac
+}
+
+main "$@"
